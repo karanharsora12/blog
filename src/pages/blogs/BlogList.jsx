953ff5ccc7761dabc33e116ffe423253978fe376
karanharsora12 <karanharsora2@gmail.com>
2024-03-26 22:44:41 +0530

    complete backend

---
+++ src/pages/blogs/BlogList.jsx
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+const BlogList = () => {
+  return (
+    <section>
+      <Container>
+        
+      </Container>
+    </section>
+  )
+}
+
+export default BlogList
